c00e667a7ea1eabe203077f977dcc762ad7112ae
Vanessa Kosiyanon <72635635+vkosiyan@users.noreply.github.com>
2021-02-05 14:35:53 -0800

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,94 @@
+$(function() {
+
+    "use strict";
+
+    var wind = $(window);
+
+
+    // scrollIt
+    $.scrollIt({
+      upKey: 38,                // key code to navigate to the next section
+      downKey: 40,              // key code to navigate to the previous section
+      easing: 'swing',          // the easing function for animation
+      scrollTime: 600,          // how long (in ms) the animation takes
+      activeClass: 'active',    // class given to the active nav element
+      onPageChange: null,       // function(pageIndex) that is called when page is changed
+      topOffset: -63            // offste (in px) for fixed top navigation
+    });
+
+
+
+    // navbar scrolling background
+    wind.on("scroll",function () {
+
+        var bodyScroll = wind.scrollTop(),
+            navbar = $(".navbar"),
+            logo = $(".navbar .logo> img");
+
+        if(bodyScroll > 100){
+
+            navbar.addClass("nav-scroll");
+            logo.attr('src', 'img/logo-dark.png');
+
+        }else{
+
+            navbar.removeClass("nav-scroll");
+            logo.attr('src', 'img/logo-light.png');
+        }
+    });
+
+
+
+    // progress bar
+    wind.on('scroll', function () {
+        $(".skills-progress span").each(function () {
+            var bottom_of_object = 
+            $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = 
+            $(window).scrollTop() + $(window).height();
+            var myVal = $(this).attr('data-value');
+            if(bottom_of_window > bottom_of_object) {
+                $(this).css({
+                  width : myVal
+                });
+            }
+        });
+    });
+
+
+
+    // sections background image from data background
+    var pageSection = $(".bg-img, section");
+    pageSection.each(function(indx){
+        
+        if ($(this).attr("data-background")){
+            $(this).css("background-image", "url(" + $(this).data("background") + ")");
+        }
+    });
+
+
+
+    // magnificPopup
+    $('.gallery').magnificPopup({
+        delegate: '.link',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+});
+
+
+// === window When Loading === //
+
+$(window).on("load",function (){
+
+    var wind = $(window);
+
+    // Preloader
+    $(".loading").fadeOut(500);
+
+
+});
